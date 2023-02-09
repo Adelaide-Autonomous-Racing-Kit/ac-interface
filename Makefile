98c1f7053479da7a98f2d35893a85fb17ed6c8f0
Adrian Orenstein <adrian.orenstein@gmail.com>
2023-02-10 08:56:58 +1030

    add matplotlib

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ ifneq ($(wildcard $(CONDA_ENV_PATH)),)
 		conda env remove -p $(CONDA_ENV_PATH); \
 	fi
 endif
-	conda create -y -p $(CONDA_ENV_PATH) opencv numpy av pyyaml python-xlib \
+	conda create -y -p $(CONDA_ENV_PATH) opencv numpy av pyyaml python-xlib matplotlib \
 		black flake8-black flake8 isort loguru pytest pytest-benchmark pytest-parallel \
 		coverage pyautogui -c conda-forge
 	$(CONDA_ACTIVATE) $(CONDA_ENV_PATH)
---
+++ src/track_gen/obj_to_vertex_csv.py
@@ -0,0 +1,29 @@
+vertices = []
+
+with open("tracks/monza.obj", "r") as file:
+    for line in file:
+        if line.startswith("v "):
+            vertex = line.split()
+            x = float(vertex[1])
+            y = float(vertex[2])
+            z = float(vertex[3])
+            vertices.append((x, y, z))
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+
+xs = [vertex[0] for vertex in vertices[::5]]
+ys = [vertex[1] for vertex in vertices[::5]]
+zs = [vertex[2] for vertex in vertices[::5]]
+
+# Because X, Y, Z and we want to maintain Z to be "up"
+ax.scatter(xs, ys, zs)
+
+ax.set_xlabel("X")
+ax.set_ylabel("Z")
+ax.set_zlabel("Y")
+
+plt.show()
