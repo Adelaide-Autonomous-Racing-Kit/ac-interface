9757ecd06f41adb2e210df3e0215ee6659ba7013
James Bockman <james@aiml.team>
2023-02-17 19:18:13 +1030

    Refactor to accomodate different layouts of AC and ACC memory

--- src/game_capture/state/shared_memory/ACC/graphics.py
+++ src/game_capture/state/shared_memory/ACC/graphics.py
@@ -1,13 +1,6 @@
 import ctypes
 
 
-class Point(ctypes.Structure):
-    _fields_ = [
-        ("x", ctypes.c_float),
-        ("y", ctypes.c_float),
-        ("z", ctypes.c_float),
-    ]
-
 
 class GraphicsSharedMemory(ctypes.Structure):
     """
---
+++ src/game_capture/state/shared_memory/utils.py
@@ -0,0 +1,8 @@
+import ctypes
+
+class Point(ctypes.Structure):
+    _fields_ = [
+        ("x", ctypes.c_float),
+        ("y", ctypes.c_float),
+        ("z", ctypes.c_float),
+    ]
