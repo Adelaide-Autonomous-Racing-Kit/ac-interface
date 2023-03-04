1e1f7ab6594ed827e1b480bd2641a99655ea8dac
James Bockman <james@bockman.dev>
2023-03-04 21:18:44 +1030

    Started semantic mapping

---
+++ src/analysis/monza/constants.py
@@ -0,0 +1,11 @@
+MESH_NAME_TO_SEMANTICS = {
+    "a": "vegetation",
+    "aa": "vegetation",
+    "b": "vegetation",
+    "c": "vegetation",
+    "f": "vegetation",
+    "g": "vegetation",
+    "h": "vegetation",
+    "l": "vegetation",
+    "m": "vegetation",
+}
