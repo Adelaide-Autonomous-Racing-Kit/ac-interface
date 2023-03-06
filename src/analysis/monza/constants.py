b2a4eebe8bad5121f23ef8fb061f3ecb74107819
James Bockman <james@aiml.team>
2023-03-07 08:45:54 +1030

    Mesh and camera to segmentation mask class implemented

--- src/analysis/monza/constants.py
+++ src/analysis/monza/constants.py
@@ -1,11 +1,138 @@
-MESH_NAME_TO_SEMANTICS = {
+from dataclasses import dataclass
+from typing import Tuple
+
+import numpy as np
+
+
+@dataclass
+class ClassInformation:
+    name: str
+    train_id: int
+    colour: Tuple[int]
+
+
+SEMANTIC_CLASSES = [
+    ClassInformation("road", 0, (84, 84, 84)),
+    ClassInformation("curb", 1, (255, 119, 51)),
+    ClassInformation("track_limit", 2, (255, 255, 255)),
+    ClassInformation("sand", 3, (255, 255, 0)),
+    ClassInformation("grass", 4, (170, 255, 128)),
+    ClassInformation("vehicle", 5, (255, 42, 0)),
+    ClassInformation("structure", 6, (153, 153, 255)),
+    ClassInformation("vegetation", 7, (0, 102, 17)),
+    ClassInformation("people", 8, (255, 179, 204)),
+    ClassInformation("void", -1, (0, 0, 0)),
+]
+
+SEMANTIC_NAME_TO_COLOUR = {info.name: info.colour for info in SEMANTIC_CLASSES}
+SEMANTIC_NAME_TO_ID = {info.name: info.train_id for info in SEMANTIC_CLASSES}
+
+COLOUR_LIST = [
+    info.colour
+    for info in sorted(SEMANTIC_CLASSES, key=lambda x: x.train_id)
+    if info.train_id > -1
+]
+COLOUR_LIST.append(SEMANTIC_CLASSES[-1].colour)
+COLOUR_LIST = np.asarray(COLOUR_LIST)
+
+MESH_NAME_TO_SEMANTIC_CLASS = {
     "a": "vegetation",
     "aa": "vegetation",
+    "accrewloda": "people",
+    "accrewlodb": "people",
+    "ambulance": "vehicle",
+    "aspgrp": "road",
+    "asphshd": "road",
+    "asphdrk": "road",
     "b": "vegetation",
+    "box": "structure",
+    "boxbottom": "structure",
+    "boxconc": "structure",
+    "boxmtlprtc": "structure",
+    "boxparall": "structure",
+    "boxparhi": "structure",
+    "boxparlow": "structure",
+    "boxparmid": "structure",
+    "bridgebase": "structure",
+    "bridgedetailhi": "structure",
+    "bridgelow": "structure",
+    "bullonshikslayer": "structure",
     "c": "vegetation",
+    "camerafacingkslayer": "structure",
+    "car": "vehicle",
+    "concpits": "road",
+    "crbshd": "curb",
+    "curbgrp": "curb",
+    "cylinder": "structure",
+    "d": "vegetation",
+    "e": "vegetation",
+    "ext": "vegetation",
     "f": "vegetation",
+    "fence": "structure",
+    "flag": "structure",
+    "flagt": "structure",
     "g": "vegetation",
+    "giuntihikslayer": "structure",
+    "grailhi": "structure",
+    "graillow": "structure",
+    "grsshd": "grass",
     "h": "vegetation",
+    "hedge": "vegetation",
+    "hi": "structure",
+    "i": "vegetation",
+    "j": "vegetation",
+    "jointmid": "structure",
+    "k": "vegetation",
     "l": "vegetation",
+    "line": "structure",
+    "loft": "structure",
+    "low": "structure",
     "m": "vegetation",
+    "mainpole": "structure",
+    "marshallhia": "people",
+    "marshallhib": "people",
+    "marshallhic": "people",
+    "marshalllowa": "people",
+    "marshalllowb": "people",
+    "marshalllowc": "people",
+    "mesh": "structure",
+    "metals": "structure",
+    "object": "structure",
+    "objectsub": "structure",
+    "padd": "structure",
+    "parab": "road",
+    "passhi": "structure",
+    "passlow": "structure",
+    "passmid": "structure",
+    "poledetailhi": "structure",
+    "poledetailmid": "structure",
+    "postbluhi": "structure",
+    "railclone": "structure",
+    "shadowserraglio": "road",
+    "shape": "structure",
+    "shaps": "structure",
+    "sand": "sand",
+    "sndgrp": "sand",
+    "standhi": "structure",
+    "standlow": "structure",
+    "standmid": "structure",
+    "structures": "structure",
+    "t": "vegetation",
+    "terrain": "structure",
+    "treed": "vegetation",
+    "treeshadow": "vegetation",
+    "tribasc": "structure",
+    "triblesmo": "structure",
+    "u": "vegetation",
+    "v": "vegetation",
+    "veichles": "vehicle",
+    "whline": "track_limit",
+    "z": "vegetation",
+    "painted": "road",
+}
+
+
+MESH_NAME_TO_ID = {
+    mesh_name: SEMANTIC_NAME_TO_ID[semantic_name]
+    for mesh_name, semantic_name in MESH_NAME_TO_SEMANTIC_CLASS.items()
 }
--- src/analysis/segmentation.py
+++ src/analysis/segmentation.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import math
 import numpy as np
+import time
 from pathlib import Path
 from typing import List
 
@@ -10,101 +11,107 @@ from loguru import logger
 
 from src.utils.load import load_game_state
 from src.track_gen.track_gen import Monza
+from src.analysis.monza.constants import (
+    MESH_NAME_TO_SEMANTIC_CLASS,
+    COLOUR_LIST,
+    MESH_NAME_TO_ID,
+)
 
-TRACK_MESH_PATH = Path.home().joinpath(Path("Documents/tracks/monza/visual_mesh.obj"))
+TRACK_MESH_PATH = Path.home().joinpath(
+    Path("Documents/tracks/monza/visual_mesh.obj")
+)
 RECORDED_DATA_PATH = Path.home().joinpath(
     Path("Documents/recordings/monza_audi_r8_lms_1/")
 )
-RECORD_FILE = "3675.bin"
-RECORD_FILE = "3325.bin"
-RECORD_FILE = "4170.bin"
-RECORD_FILE = "5090.bin"
 
 
-def get_scene():
-    scene = trimesh.Scene()
+class SegmentationGenerator:
+    def __init__(self):
+        self.__setup_scene()
+        self.__setup_collision_mesh()
 
-    track = Monza(TRACK_MESH_PATH)
-    logger.info(f"{track.named_meshes.keys()}")
-    for name in track.named_meshes.keys():
-        meshes = track.named_meshes[name]
-        meshes = [mesh for mesh in meshes.values()]
-        combined_mesh = trimesh.util.concatenate(meshes)
-        scene.add_geometry(
-            combined_mesh,
-            node_name=name,
+    def __setup_scene(self):
+        logger.info("Loading track meshes...")
+        scene = trimesh.Scene()
+        track = Monza(TRACK_MESH_PATH)
+        for name in MESH_NAME_TO_SEMANTIC_CLASS.keys():
+            meshes = track.named_meshes[name]
+            meshes = [mesh for mesh in meshes.values()]
+            combined_mesh = trimesh.util.concatenate(meshes)
+            scene.add_geometry(
+                combined_mesh,
+                node_name=name,
+            )
+        scene.triangles_to_ids = np.asarray(
+            [MESH_NAME_TO_ID[name] for name in scene.triangles_node]
         )
+        self._scene = scene
+        logger.info("Track mesh loaded")
 
-    logger.info(len(scene.geometry))
-    return scene
+    def __setup_collision_mesh(self):
+        meshes = [mesh for mesh in self._scene.geometry.values()]
+        mesh = trimesh.util.concatenate(meshes)
+        self._mesh = trimesh.ray.ray_pyembree.RayMeshIntersector(mesh)
 
+    def adjust_camera(self, state_filename: str):
+        state = load_game_state(
+            os.path.join(RECORDED_DATA_PATH, state_filename + ".bin")
+        )
+        angles = [state["pitch"], state["heading"], state["roll"]]
+        angles[1] = angles[1] + math.pi
+        distance = 0.0
+        center = [
+            state["ego_location_x"],
+            state["ego_location_y"] + state["centre_of_gravity_height"],
+            state["ego_location_z"],
+        ]
+        fov = (91, 60)
+        resolution = (1920, 1080)
+        self._scene.set_camera(angles, distance, center, resolution, fov)
+        logger.info(f"Moved camera to: {center} with rotation {angles}")
 
-def setup_camera(scene, state_filename: str):
-    state = load_game_state(os.path.join(RECORDED_DATA_PATH, state_filename + ".bin"))
-    angles = [state["pitch"], state["heading"], state["roll"]]
-    logger.info(angles)
-    angles[1] = angles[1] + math.pi
-    logger.info(angles)
-    distance = 0.0
-    center = (
-        state["ego_location_x"],
-        state["ego_location_y"] + state["centre_of_gravity_height"],
-        state["ego_location_z"],
-    )
-    fov = (91, 60)
-    resolution = (1920, 1080)
-    scene.set_camera(angles, distance, center, resolution, fov)
+    def _get_triangle_ray_intersections(self) -> List[int]:
+        origins, vectors, _ = self._scene.camera_rays()
+        tri_indexes = self._mesh.intersects_first(origins, vectors)
+        pixel_ids = np.empty(tri_indexes.size, dtype=np.int32)
+        pixel_ids[tri_indexes == -1] = -1
+        pixel_ids[tri_indexes != -1] = self._scene.triangles_to_ids[
+            tri_indexes[tri_indexes != -1]
+        ]
+        return pixel_ids
 
-
-def get_triangle_ray_intersections(scene) -> List[int]:
-    mesh = trimesh.util.concatenate([mesh for mesh in scene.geometry.values()])
-    ray_mesh_int = trimesh.ray.ray_pyembree.RayMeshIntersector(mesh)
-    origins, vectors, _ = scene.camera_rays()
-    tri_indexs = ray_mesh_int.intersects_first(origins, vectors)
-    return scene.triangles_node[tri_indexs]
-
-
-import matplotlib.cm as cm
-import random
-
-
-def make_colour_map(scene):
-    names = [name for name in scene.triangles_node]
-    cmap = cm.get_cmap("viridis")
-    values = np.arange(len(names))
-    # colours = cmap(values)
-    # colour_map = {name: colour[:3] for name, colour in zip(names, colours)}
-    colour_map = {
-        name: (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        for name in names
-    }
-    logger.info(colour_map)
-    return colour_map
-
-
-def show_colour_map(scene):
-    colour_map = make_colour_map(scene)
-    pixel_geometry_names = get_triangle_ray_intersections(scene)
-    to_plot = (
-        np.array(
-            [colour_map[pixel_name] for pixel_name in pixel_geometry_names]
+    def save_colour_map(self, record_number: str):
+        start_time = time.time()
+        pixel_ids = self._get_triangle_ray_intersections()
+        to_plot = np.array(
+            COLOUR_LIST[pixel_ids],
+            dtype=np.uint8,
         ).reshape(1920, 1080, 3)
-        * 255
-    )
-    # to_plot = np.transpose(to_plot, (0, 1))
-
-    to_plot = np.rot90(to_plot)
-    # to_plot = np.flip(to_plot)
-    to_plot = np.flipud(to_plot)
-    to_plot = to_plot.astype(np.uint8)
-    cv2.imshow("hello", to_plot)
-    cv2.waitKey()
+        to_plot = cv2.cvtColor(to_plot, cv2.COLOR_RGB2BGR)
+        to_plot = np.rot90(to_plot)
+        to_plot = np.flipud(to_plot)
+        cv2.imwrite(
+            os.path.join(RECORDED_DATA_PATH, f"{record_number}-colour.png"),
+            to_plot,
+        )
+        logger.info(
+            f"Saved semantic colour map in {time.time() - start_time}s"
+        )
 
 
 def main():
-    scene = get_scene()
-    setup_camera(scene, "5090")
-    show_colour_map(scene)
+    segmentation_generator = SegmentationGenerator()
+    records = sorted(
+        [
+            record[:-4]
+            for record in os.listdir(RECORDED_DATA_PATH)
+            if record[-3:] == "bin"
+        ],
+        key=lambda x: int(x),
+    )
+    for record in records:
+        segmentation_generator.adjust_camera(record)
+        segmentation_generator.save_colour_map(record)
 
 
 if __name__ == "__main__":
