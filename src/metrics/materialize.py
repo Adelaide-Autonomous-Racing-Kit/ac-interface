9ca52ce7bb21ee4efbed54526b45a24a7162327c
Adrian Orenstein <adrian.orenstein@gmail.com>
2023-03-20 20:13:55 +1030

    materialize

---
+++ src/metrics/materialize.py
@@ -0,0 +1,38 @@
+"""
+pip install psycopg # psycopg-3.1.8-py3-none-any.whl
+
+# turns on postgres and materialize server, we've done the connection via docker compose
+docker compose up -d src/metrics/docker-compose.yml
+
+# turns off postgres and materialize server
+docker compose down src/metrics/docker-compose.yml
+"""
+
+import psycopg
+import sys
+
+# Establish connection to Postgres server
+conn = psycopg.connect(
+    dbname="my_db",
+    user="user",
+    password="pass",
+    host="localhost",
+    port="5432",
+)
+conn.autocommit = True
+
+# Create a cursor to interact with the database
+cur = conn.cursor()
+
+# Execute a SQL command to show the wal_level setting
+cur.execute("SHOW wal_level")
+
+# Fetch the result
+result = cur.fetchone()
+
+# Print the result
+print(f"wal_level: {result[0]}")
+
+# Close the cursor and connection
+cur.close()
+conn.close()
