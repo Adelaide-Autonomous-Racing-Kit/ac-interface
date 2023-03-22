398b8a6723f996d776dbc4f164260c1cc23e8dc1
James Bockman <james@bockman.dev>
2023-03-22 17:41:16 +1030

    Progress update, cleaned up variables, made config files adjusted text video, made the class properly generate a segmentation dataset

--- src/analysis/test_video.py
+++ src/analysis/test_video.py
@@ -1,51 +1,28 @@
 import os
-import math
+from pathlib import Path
 
 import cv2
 import numpy as np
 from tqdm import tqdm
 
-from src.analysis.segmentation import RECORDED_DATA_PATH
-from src.utils.load import load_game_state
+from src.analysis.segmentation import DataGenerator
 
 
 def main():
-    records = sorted(
-        [
-            record[:-4]
-            for record in os.listdir(RECORDED_DATA_PATH)
-            if record[-3:] == "bin"
-        ],
-        key=lambda x: int(x),
-    )
-    video_path = os.path.join(RECORDED_DATA_PATH, "monza_audi_r8.avi")
+    root_path = Path(os.path.dirname(__file__))
+    config_path = root_path.joinpath("monza/config.yaml")
+    data_generator = DataGenerator(config_path)
+    video_path = data_generator.output_path.joinpath("monza_audi_r8.avi")
+    image_size = data_generator.image_size
+    video_size = (2 * image_size[0], image_size[1])
     fourcc = cv2.VideoWriter_fourcc(*"XVID")
-    output = cv2.VideoWriter(video_path, fourcc, 30, (2 * 1920, 1080))
-    for record in tqdm(records):
-        root_path = os.path.join(RECORDED_DATA_PATH, record)
+    frame_rate = 30 // data_generator._config["sample_every"]
+    output = cv2.VideoWriter(str(video_path), fourcc, frame_rate, video_size)
+    for record in tqdm(data_generator._get_subsample()):
+        root_path = str(data_generator.recording_path.joinpath(record))
         mask = cv2.imread(root_path + "-colour.png")
         image = cv2.imread(root_path + ".jpeg")
-        state = load_game_state(root_path + ".bin")
         to_show = np.hstack([image, mask])
-        angles = [state["pitch"], state["heading"], state["roll"]]
-        angles[1] = angles[1] + math.pi
-        center = [
-            state["ego_location_x"],
-            state["ego_location_y"] + state["centre_of_gravity_height"],
-            state["ego_location_z"],
-        ]
-        text = f"(X,Y,Z): {center} (pitch, yaw, roll): {angles}"
-        to_show = cv2.putText(
-            to_show,
-            text,
-            (800, 30),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 0, 255),
-            3,
-        )
-        cv2.imshow("hi", to_show)
-        cv2.waitKey(1)
         output.write(to_show)
     output.release()
 
