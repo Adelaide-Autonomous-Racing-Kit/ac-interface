8b0c2af6e69341d6bd366f5301cb20fdd04b2460
James Bockman <39457784+XDynames@users.noreply.github.com>
2024-10-21 11:47:27 +1030

    Runtime monitoring (#87)
    * Fixed order of magnitude error in ms conversion, changed fps target, made SystemMonitor more usable

--- src/aci/game_capture/video/pyav_capture.py
+++ src/aci/game_capture/video/pyav_capture.py
@@ -4,7 +4,7 @@ from typing import Dict
 
 from aci.utils import display
 from aci.utils.os import get_display_input, get_file_format, get_sanitised_os_name
-from aci.utils.system_monitor import System_Monitor
+from aci.utils.system_monitor import System_Monitor, track_runtime
 import av
 from loguru import logger
 import numpy as np
@@ -97,12 +97,19 @@ class ImageStream:
 
     def _run(self):
         while self._is_running:
-            frame = next(self._frame_generator)
-            if not self._is_duplicate_frame(frame):
-                bgr0_image = self._get_BGR0_image_from_frame(frame)
-                self._latest_image = bgr0_image
-                self._latest_dts = frame.dts
-                self._is_new_frame = True
+            self._frame_capture_work()
+
+    @track_runtime(System_Monitor)
+    def _frame_capture_work(self):
+        frame = self._get_next_frame()
+        if not self._is_duplicate_frame(frame):
+            bgr0_image = self._get_BGR0_image_from_frame(frame)
+            self._latest_image = bgr0_image
+            self._latest_dts = frame.dts
+            self._is_new_frame = True
+
+    def _get_next_frame(self):
+        return next(self._frame_generator)
 
     def _is_duplicate_frame(self, frame) -> bool:
         return frame.dts == self._latest_dts
--- src/aci/utils/system_monitor.py
+++ src/aci/utils/system_monitor.py
@@ -5,38 +5,95 @@ import time
 from loguru import logger
 import numpy as np
 
+LOG_EVERY_N = 5000
+N_EXECUTIONS_TO_TRACK = 1000
+
+
+class FunctionStats:
+    def __init__(self):
+        self._execution_times = -1.0 * np.ones(N_EXECUTIONS_TO_TRACK, dtype=np.float32)
+        self._n_tracked = 0
+        self._start_time = time.time_ns()
+
+    def add_execution_times(self, time_ms: float):
+        index = self._n_tracked % N_EXECUTIONS_TO_TRACK
+        self._execution_times[index] = time_ms
+        self._n_tracked += 1
+
+    @property
+    def average_execution_time(self) -> float:
+        return np.mean(self.execution_times)
+
+    @property
+    def standard_deviation_of_execution_time(self) -> float:
+        return np.std(self.execution_times)
+
+    @property
+    def itteration_per_second(self) -> float:
+        time_elapsed = (time.time_ns() - self._start_time) * 1e-9
+        return self._n_tracked / time_elapsed
+
+    @property
+    def execution_times(self) -> np.array:
+        if self._n_tracked < N_EXECUTIONS_TO_TRACK:
+            execution_times = self._execution_times[self._execution_times > 0]
+        else:
+            execution_times = self._execution_times
+        return execution_times
+
 
 class SystemMonitor:
-    def __init__(self):
-        self.runtimes = defaultdict(list)
+    def __init__(self, log_every_n: int):
+        self.runtimes = defaultdict(FunctionStats)
+        self._n_iterations = 0
+        self._log_every_n = log_every_n
 
     def add_function_runtime(self, function_name: str, runtime: float):
-        self.runtimes[function_name].append(runtime)
+        self.runtimes[function_name].add_execution_times(runtime)
 
-    def average_runtime(self, function_name: str) -> float:
-        return np.mean(self.runtimes[function_name])
+    def maybe_log_function_itterations_per_second(self):
+        if self._is_logging_interval:
+            self._log_function_itterations_per_second()
+        self._n_iterations += 1
 
-    def standard_deviation_of_runtime(self, function_name: str) -> float:
-        return np.std(self.runtimes[function_name])
+    @property
+    def _is_logging_interval(self) -> bool:
+        return self._n_iterations % self._log_every_n == 0
 
     def log_function_runtimes_times(self):
         for key in self.runtimes.keys():
-            average_runtime = self.average_runtime(key)
-            std_dev = self.standard_deviation_of_runtime(key)
+            function_stats = self.runtimes[key]
+            average_runtime = function_stats.average_execution_time
+            std_dev = function_stats.standard_deviation_of_execution_time
             logger.info(f"{key} runs: {average_runtime:.2f} ± {std_dev:.2f} ms")
 
+    def _log_function_itterations_per_second(self):
+        for key in self.runtimes.keys():
+            function_stats = self.runtimes[key]
+            average_runtime = function_stats.average_execution_time
+            std_dev = function_stats.standard_deviation_of_execution_time
+            observed_it_per_s = function_stats.itteration_per_second
+            message = f"{key}: \n"
+            message += f"Average runtime: {average_runtime:.2f} ± {std_dev:.2f} ms\n"
+            message += f"Theoretical max: {(1000.0 / average_runtime):.2f} it/s\n"
+            message += f"Observed: {observed_it_per_s:.2f} it/s"
+            logger.debug(message)
 
-System_Monitor = SystemMonitor()
+
+System_Monitor = SystemMonitor(LOG_EVERY_N)
 
 
-def track_runtime(function):
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        t1 = time.time()
-        result = function(*args, **kwargs)
-        t2 = time.time()
-        name = f"{function.__module__}.{function.__name__}"
-        System_Monitor.add_function_runtime(name, (t2 - t1) * 10e3)
-        return result
+def track_runtime(system_monitor: SystemMonitor):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            t1 = time.time_ns()
+            result = function(*args, **kwargs)
+            t2 = time.time_ns()
+            name = f"{function.__module__}.{function.__name__}"
+            system_monitor.add_function_runtime(name, (t2 - t1) * 1e-6)
+            return result
 
-    return wrapper
+        return wrapper
+
+    return decorator
