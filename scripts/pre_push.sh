863369c7470434ec330aed75a1450521e4097e2c
XDynames <james.bockman@live.com.au>
2026-03-02 09:55:31 +1030

    Updated precommit hooks for new python version

--- scripts/pre_push.sh
+++ scripts/pre_push.sh
@@ -4,4 +4,4 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 # Run pre-commit hooks with the specified Python interpreter
-"$REPO_ROOT/envs/bin/python3.9" -m pre_commit run --config "$REPO_ROOT/.pre-push-config.yaml" --all-files --hook-stage push
+"$REPO_ROOT/envs/bin/python3.11" -m pre_commit run --config "$REPO_ROOT/.pre-push-config.yaml" --all-files --hook-stage push
